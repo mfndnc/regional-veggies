331342c02b783d0ce97df0597dc5445dc115170b
Marcia Faria <mfndnc@gmail.com>
2021-05-17 16:25:15 +0200

    cleaning

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -14,6 +14,7 @@ export default function Explore() {
   const [loading, setLoading] = useState(true);
   const [fullAddress, setFullAddress] = useState([]);
   const [selected, setSelected] = useState(null);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     api
@@ -23,6 +24,8 @@ export default function Explore() {
       })
       .finally(() => setLoading(false));
   }, []);
+
+  const handleChange = (e) => setQuery(e.target.value);
 
   const doChildtoParent = (args) => {
     console.log(args);
@@ -39,31 +42,79 @@ export default function Explore() {
   if (loading) return <h3>Loading...</h3>;
 
   return (
-    <div className="container-xl">
-      <div className="row">
-        <div className="col">
-          <div className="panel-body">
-            <ul className="list-group explore">
-              {fullAddress.map((addr) => (
-                <li
-                  className="list-group-item"
-                  key={addr._id}
-                  data-id={addr._id}
-                  onClick={clickNameList}
-                >
-                  {addr.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="col">{selected && <div>{selected.name}</div>}</div>
-      </div>
+    <div className="container-fluid">
+      {''}
       <div className="row">
         <div className="col" style={containerStyle}></div>
-      </div>
+      </div>{' '}
+      <div className="row">
+        <div className="colgrid col">
+          <div
+            className={`card-group modern row row-cols-1 row-cols-md-${
+              selected ? '2' : '1'
+            }`}
+          >
+            {' '}
+            <div className="col mb-4">
+              <div className="card h-100 text-left">
+                <div className="card-body-wrap h-100 no-image">
+                  <div className="card-body">
+                    <h4 className="card-title">Search Addresses</h4>
+                    <h5 className="card-subtitle mb-2 text-muted">
+                      <div className="form-group">
+                        <div className="form-label-group">
+                          <input
+                            placeholder="Search"
+                            className="form-control"
+                            id="query"
+                            type="text"
+                            name="query"
+                            value={query}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </h5>
+                    <div className="card-text">
+                      <ul className="list-group explore">
+                        {fullAddress.map((addr) => (
+                          <li
+                            className="list-group-item"
+                            key={addr._id}
+                            data-id={addr._id}
+                            onClick={clickNameList}
+                          >
+                            {addr.name}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>{' '}
+            {selected && (
+              <div className="col mb-4">
+                <div className="card h-100 text-left">
+                  <div className="card-body-wrap h-100 no-image">
+                    <div className="card-body">
+                      <h4 className="card-title">Selected</h4>
+                      <h5 className="card-subtitle mb-2 text-muted">
+                        {selected.name}
+                      </h5>
+                      <div className="card-text">Lorem ipsum dolor sit.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}{' '}
+          </div>
+        </div>
+      </div>{' '}
     </div>
   );
 }
 
+// <div className="col" style={containerStyle}></div>
+// <div className="col"><GoogleMap markers={fullAddress} childtoParent={doChildtoParent} selectedMarker={selected} /></div>
 //<GoogleMap markers={fullAddress} childtoParent={doChildtoParent} selectedMarker={selected} />
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -46,54 +46,59 @@ export default function Profile() {
   const addressAccordion = fullAddress.map((addr, idx) => {
     let ddd = `id${idx}`;
     return (
-      <div className="col-lg-6" key={`addr${idx}`}>
-        <div className="card ">
-          <Collapse in={!openAddressObj[`id${idx}`]}>
-            <div id={`collapseAddressY${idx}`}>
-              <div className="card-header">
-                Address - {addr.name ? addr.name : idx + 1}
+      <div className="col mb-4" key={`addr${idx}`}>
+        <div className="card h-100">
+          <div className="card-body-wrap h-100 no-image">
+            <Collapse in={!openAddressObj[`id${idx}`]}>
+              <div id={`collapseAddressY${idx}`}>
+                <div className="card-header">
+                  Address - {addr.name ? addr.name : idx + 1}
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{addr.city}</h5>
+                  <p className="card-text">
+                    {addr.street} {addr.suite}
+                  </p>
+                  <p className="card-text">
+                    {addr.zipcode} {addr.city}
+                  </p>
+                  <p className="card-text">
+                    {addr.phone} {addr.skype} {addr.whatsapp} {addr.twitter}
+                  </p>
+                </div>
               </div>
-              <div className="card-body">
-                <h5 className="card-title">{addr.city}</h5>
-                <p className="card-text">
-                  {addr.street} {addr.suite}
-                </p>
-                <p className="card-text">
-                  {addr.zipcode} {addr.city}
-                </p>
-                <p className="card-text">
-                  {addr.phone} {addr.skype} {addr.whatsapp} {addr.twitter}
-                </p>
-              </div>
-            </div>
-          </Collapse>
-          <div className={`card-${openAddressObj[ddd] ? 'header' : 'footer'}`}>
-            <Button
-              onClick={() =>
-                setOpenAddressObj((prevSt) => ({
-                  ...prevSt,
-                  [ddd]: !prevSt[ddd],
-                }))
-              }
-              aria-controls={`collapseAddress${idx} collapseAddressY${idx}`}
-              aria-expanded={openAddressObj[ddd]}
+            </Collapse>
+            <div
+              className={`card-${openAddressObj[ddd] ? 'header' : 'footer'}`}
             >
-              {openAddressObj[ddd] ? 'Close' : 'Mofidy'}
-            </Button>
-          </div>
-          <Collapse in={openAddressObj[`id${idx}`]}>
-            <div id={`collapseAddress${idx}`}>
-              {openAddressObj[`id${idx}`] && (
-                <AddressForm
-                  accordion
-                  onSave={afterChildSave}
-                  closeAccordion={doCloseAccordionAddress}
-                  closeAccordionArgs={ddd}
-                  addressId={addr._id}
-                />
-              )}
+              <Button
+                variant="secondary"
+                onClick={() =>
+                  setOpenAddressObj((prevSt) => ({
+                    ...prevSt,
+                    [ddd]: !prevSt[ddd],
+                  }))
+                }
+                aria-controls={`collapseAddress${idx} collapseAddressY${idx}`}
+                aria-expanded={openAddressObj[ddd]}
+              >
+                {openAddressObj[ddd] ? 'Close' : 'Mofidy'}
+              </Button>
             </div>
-          </Collapse>
+            <Collapse in={openAddressObj[`id${idx}`]}>
+              <div id={`collapseAddress${idx}`}>
+                {openAddressObj[`id${idx}`] && (
+                  <AddressForm
+                    accordion
+                    onSave={afterChildSave}
+                    closeAccordion={doCloseAccordionAddress}
+                    closeAccordionArgs={ddd}
+                    addressId={addr._id}
+                  />
+                )}
+              </div>
+            </Collapse>
+          </div>
         </div>
       </div>
     );
@@ -103,73 +108,94 @@ export default function Profile() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-6">
-          <div className="card ">
-            <Collapse in={!openProfile}>
-              <div id="collapseProfileY">
-                <div className="card-header">Personal Data</div>
-                <div className="card-body">
-                  <h5 className="card-title">{authObj.name}</h5>
-                  <p className="card-text">{authObj.note}</p>
-                  <p className="card-text">
-                    {authObj.email} {authObj.phone} ...
-                  </p>
-                  {!openAddress && (
+        <div className="colgrid col">
+          <div className="card-group modern row row-cols-1 row-cols-md-1">
+            <div className="col mb-4">
+              <div className="card h-100">
+                <div className="card-body-wrap h-100 no-image ">
+                  <Collapse in={!openProfile}>
+                    <div id="collapseProfileY">
+                      <div className="card-header">Personal Data</div>
+                      <div className="card-body">
+                        <h5 className="card-title">{authObj.name}</h5>
+                        <p className="card-text">{authObj.note}</p>
+                        <p className="card-text">
+                          {authObj.email} {authObj.phone} ...
+                        </p>
+                        {!openAddress && (
+                          <Button
+                            variant="info"
+                            size="sm"
+                            onClick={() => setOpenAddress(!openAddress)}
+                            aria-controls="collapseAddressCard"
+                            aria-expanded={openAddress}
+                          >
+                            New Address
+                          </Button>
+                        )}
+                      </div>
+                    </div>
+                  </Collapse>
+                  <div className={`card-${openProfile ? 'header' : 'footer'}`}>
                     <Button
-                      onClick={() => setOpenAddress(!openAddress)}
-                      aria-controls="collapseAddressCard"
-                      aria-expanded={openAddress}
+                      variant="secondary"
+                      onClick={() => setOpenProfile(!openProfile)}
+                      aria-controls="collapseProfile collapseProfileY"
+                      aria-expanded={openProfile}
                     >
-                      New Address
+                      {openProfile ? 'Close' : 'Mofidy'}
                     </Button>
-                  )}
+                  </div>
+                  <Collapse in={openProfile}>
+                    <div id="collapseProfile">
+                      {openProfile && (
+                        <ProfileForm
+                          accordion
+                          onSave={afterChildSave}
+                          closeAccordion={setOpenProfile}
+                        />
+                      )}
+                    </div>
+                  </Collapse>
                 </div>
               </div>
-            </Collapse>
-            <div className={`card-${openProfile ? 'header' : 'footer'}`}>
-              <Button
-                onClick={() => setOpenProfile(!openProfile)}
-                aria-controls="collapseProfile collapseProfileY"
-                aria-expanded={openProfile}
-              >
-                {openProfile ? 'Close' : 'Mofidy'}
-              </Button>
             </div>
-            <Collapse in={openProfile}>
-              <div id="collapseProfile">
-                {openProfile && (
-                  <ProfileForm
-                    accordion
-                    onSave={afterChildSave}
-                    closeAccordion={setOpenProfile}
-                  />
-                )}
-              </div>
-            </Collapse>
           </div>
         </div>
       </div>
 
       <Collapse in={openAddress}>
         <div id="collapseAddressCard" className="row">
-          <div className="col-lg-12">
-            <div className="card ">
-              <div className="card-header">New Address</div>
-              <div className="card-body">
-                {openAddress && (
-                  <AddressForm
-                    accordion
-                    onSave={afterChildSave}
-                    closeAccordion={setOpenAddress}
-                  />
-                )}
+          <div className="colgrid col">
+            <div className="card-group modern row row-cols-1 row-cols-md-1">
+              <div className="col mb-4">
+                <div className="card h-100">
+                  <div className="card-body-wrap h-100 no-image">
+                    <div className="card-header">New Address</div>
+                    <div className="card-body">
+                      {openAddress && (
+                        <AddressForm
+                          accordion
+                          onSave={afterChildSave}
+                          closeAccordion={setOpenAddress}
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </Collapse>
 
-      <div className="row">{addressAccordion}</div>
+      <div className="row">
+        <div className="colgrid col">
+          <div className="card-group modern row row-cols-1 row-cols-md-2">
+            {addressAccordion}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
